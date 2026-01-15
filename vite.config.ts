ff3a18dbf37bd3bf4944a00da1d32c91b869eb1f
User <user@example.com>
2026-01-15 22:12:10 +0800

    fix: use relative base path for Vercel support

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Base path for GitHub Pages deployment
-  base: '/keep-fit/',
+  // Base path (relative for broad compatibility: Vercel & GH Pages)
+  base: './',
   build: {
     outDir: 'dist',
   }
